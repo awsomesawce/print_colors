26505f5bf49f4d2e5ecd4910072ba679d94b6002
awsomesawce <69673640+awsomesawce@users.noreply.github.com>
2024-01-31 23:58:25 -0500

    feat: :construction: Added markdown console renderer, renamed deno.json to jsonc.
    deno.jsonc because its nice to be able to write comments if needed.

---
+++ MD.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S deno run -A
+
+import * as md from "https://deno.land/x/charmd@v0.0.2/mod.ts";
+import {demoText} from "https://deno.land/x/charmd@v0.0.2/example.ts"
+
+export const renderFile = async (pth: string): Promise<void> => {
+  const str = await Deno.readTextFile(pth);
+  const rendered = md.renderMarkdown(str);
+  console.log(rendered)
+}
+
+if (import.meta.main) {
+  await renderFile('README.md')
+}
---
+++ deps.ts
@@ -0,0 +1,7 @@
+import * as md from "https://deno.land/x/charmd@v0.0.2/mod.ts";
+import * as gfm from "$x/gfm@0.2.5/mod.ts";
+
+
+export {
+  md, gfm
+}
